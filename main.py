063af49656b6359cced4e0405b47908561863654
saleh <saleh.nazih.dev@gmail.com>
2020-11-20 20:36:26 -0300

    Implement Parse Data
    Co-authored-by: Matheus Blanco <msallesblanco@gmail.com>
    Co-authored-by: Pedro Feo <pheofo@gmail.com>

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import re
+import numpy
+
 from exceptions.ArquivoNaoEncontradoException import ArquivoNaoEncontradoException
 from exceptions.DelimitadorInvalidoException import DelimitadorInvalidoException
 from exceptions.EscritaNaoPermitidaException import EscritaNaoPermitidaException
 from exceptions.FormatoInvalidoException import FormatoInvalidoException
 
-from test_data import expected_mock1
 
 def read_file(file_name):
     try:
@@ -32,7 +34,32 @@ def sequence_format(exit_format):
         raise FormatoInvalidoException(exit_format)
 
 def parse_data(content, delimit, exit_format):
-    return expected_mock1
+    content = re.sub(r"\n---------- Evolution \d+ ----------", "---", content)
+    lines = content.split('---')
+    lines = list(filter(lambda a: a != '', lines))
+    line_content = []
+    index = 0
+    for index, line in enumerate(lines, start=1):
+        values = line.split('\n')
+        values.insert(0, index)
+        values = list(filter(lambda a: a != '', values))
+        line_content.append(values)
+
+    sequence_content = ''
+    if exit_format == 'c':
+        line_content = numpy.rot90(line_content, 3)
+        line_content = numpy.fliplr(line_content)
+        line_content = line_content.tolist()
+    
+    for content in line_content:
+        for num, item in enumerate(content, start=1):
+            sequence_content += str(item)
+            if len(content) != num:
+                sequence_content += delimit
+        
+        sequence_content += '\n'
+    
+    return sequence_content   
 
 def main():
     print("Hello World")
