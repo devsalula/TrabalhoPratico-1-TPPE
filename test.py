3d9a223514de1f4cb40b7bb50af43b060a40379b
saleh <saleh.nazih.dev@gmail.com>
2020-11-20 17:41:44 -0300

    Refactor test_read_file to parameterized
    Co-authored-by: Matheus Blanco <msallesblanco@gmail.com>
    Co-authored-by: Pedro Feo <pheofo@gmail.com>

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ import pytest
 from main import read_file
 from exceptions.ArquivoNaoEncontradoException import ArquivoNaoEncontradoException
 
-def test_read_file():
-    file_name = "./assets/test.out"
-    file = read_file(file_name)
-    assert file == 'Hello World'
+@pytest.mark.parametrize("test_input,expected", [('./assets/test.out', 'Hello World'), ('./assets/test1.out', 'Ola Mundo'), ('./assets/test2.out', 'Hola Mundo')])
+def test_read_file(test_input, expected):
+    file = read_file(test_input)
+    assert file == expected
 
 def test_file_not_found():
     with pytest.raises(ArquivoNaoEncontradoException):
