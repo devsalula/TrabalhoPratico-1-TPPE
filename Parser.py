2b2d9cd7d9a6353a842c19cef08ca978e98def3c
saleh <saleh.nazih.dev@gmail.com>
2020-12-04 17:17:30 -0300

    [Refact] Extrair Metodo Origem: Parser.py Impactos: Parser.py, write_response, extract_filename Explicacao: Refatoracao de extrair metodo aplicada no metodo write_response a fim de reduzir a quantidade de responsabilidade da write_response retirando a sua responsabilidade de extrair o nome do arquivo.
    Co-authored-by: Matheus Blanco <msallesblanco@gmail.com>
    Co-authored-by: Pedro Feo <pheofo@gmail.com>

--- Parser.py
+++ Parser.py
@@ -15,14 +15,14 @@ class Parser:
         try:
             file = open(filename)
             self.content = file.read()
-            self.extract_filename(filename)
+            self.filename = self.extract_filename(filename, -1, '/')
         except:
             raise ArquivoNaoEncontradoException(filename)
 
-    def extract_filename(self, filename):
-        filename_array = filename.split('/')
-        filename = filename_array[-1]
-        self.filename = filename
+    def extract_filename(self, filename, position, character_split):
+        filename_array = filename.split(character_split)
+        filename = filename_array[position]
+        return filename
 
     def delimiter_input(self, delimiter):
         if len(delimiter) == 1:
@@ -38,7 +38,7 @@ class Parser:
             self.path = path
         else:
             raise EscritaNaoPermitidaException(path)
-    
+
     def sequence_format(self, exit_format):
         if exit_format == 'c' or exit_format == 'l':
             self.exit_format = exit_format
@@ -68,15 +68,14 @@ class Parser:
                 sequence_content += str(item)
                 if len(content) != num:
                     sequence_content += self.delimiter
-            
+
             sequence_content += '\n'
-        
+
         self.parsed_data = sequence_content
 
     def write_response(self):
         try:
-            filename_array = self.filename.split('.')
-            filename = filename_array[0]
+            filename = self.extract_filename(self.filename, 0, '.')
             file = open(self.path + filename + 'Tab.out', "w")
             file.write(self.parsed_data)
             file.close()
